55654780161bbacc28bb165aa4754b750c378eac
Saloni Gour <salonigour100@gmail.com>
2023-02-02 18:40:18 +0530

    home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,65 +1,72 @@
 import React from "react";
 import { BsFacebook, BsInstagram, BsGoogle, BsTwitter } from "react-icons/bs";
+import { Link } from "react-router-dom";
 function Footer() {
   return (
-    <div>
-      <div class="footer">
-        <div class="footer_top">
-          <div class="top top1">
-            <div className="icon">
-              <a href="#">WEBSHOP</a>
-            </div>
-            <p>Thoughtful development for growing business.</p>
+    <footer>
+      <div class="footer_top">
+        <div>
+          <div>
+            <Link to="/" className="logo">
+              WEB
+              <span style={{ color: "white" }} className="x">
+                X
+              </span>
+            </Link>
           </div>
-          <div className="top top2">
-            <p>For more insights do subscribe.</p>
-            <form>
-              <input
-                className="email"
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter your email"
-              />
-              <input className="sub_button" type="submit" value="Subscribe" />
-            </form>
-          </div>
-          <div className="top top3">
-            <p>Follow us</p>
-            <div className="icons">
-              <a href="#">
-                <BsFacebook color="white" size="20px" />
-              </a>
-              <a href="#">
-                <BsInstagram color="white" size="20px" />
-              </a>
-              <a href="#">
-                <BsGoogle color="white" size="20px" />
-              </a>
-              <a href="#">
-                <BsTwitter color="white" size="20px" />
-              </a>
-            </div>
-          </div>
-          <div className="top top4">
-            <p>
-              call us
-              <br />
-              <br />
-              91 854162537
-            </p>
+          <div>For more insights do subscribe.</div>
+        </div>
+
+        <div>
+          <div>For more insights</div>
+          <br />
+          <form>
+            <input
+              className="email"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+            />
+
+            <div className="login_button">LOG IN</div>
+          </form>
+        </div>
+
+        <div className="">
+          <p>Follow us</p>
+          <div className="icons">
+            <a href="#">
+              <BsFacebook color="white" size="30px" />
+            </a>
+            <a href="#">
+              <BsInstagram color="white" size="30px" />
+            </a>
+            <a href="#">
+              <BsGoogle color="white" size="30px" />
+            </a>
+            <a href="#">
+              <BsTwitter color="white" size="30px" />
+            </a>
           </div>
         </div>
-        <hr />
-        <div className="footer_bottom">
-          <div className="bottom1">
-            2020 Creedo,Harvey Specter Agency Newyork,USA, All Rights reserved
-          </div>
-
-          <div clasName="bottom2">privacy policy terms and condition</div>
+        <div className="">
+          <p>
+            call us
+            <br />
+            91 854162537
+          </p>
         </div>
       </div>
-    </div>
+      <hr />
+      <div className="footer_bottom">
+        <div className="">
+          2020 Creedo,Harvey Specter Agency Newyork,USA, All Rights reserved
+        </div>
+
+        <div clasName="">privacy policy terms and condition</div>
+      </div>
+    </footer>
   );
 }
 
